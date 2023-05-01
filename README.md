8ca77912bee73c810be9f0eed149fade1a2fb757
tintin10q <tintin10q@hotmail.com>
2023-05-01 02:24:22 +0200

    Initial commit
    Signed-off-by: Quinten Cabo <tintin10q@hotmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # simple-indexedDB
-A simple indexedDB wrapper
+
+A simple indexedDB wrapper.
+
+## Usage
+
+```js
+import IndexeDBObjectStore from "IndexeDBObjectStore"
+// or just copy the code into your file its only ~200 lines of code
+
+const store = await new IndexeDBObjectStore("my-db", "my-store")
+await store.put("test", 137)
+console.log(await store.get("test")) // 137
+```
+
+## Motivation
+
+There is a bunch of annoying ugly boilerplate involved when dealing
+with [IndexedDB API](https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API). When dealing with the IndexedDB
+you want an instance of the [IDBObjectStore](https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore) class. But
+to get there one has to deal with the aforementioned boilerplate:
+
+```js 
+// writing and reading indexdb in vanilla js
+let db;
+const DBOpenRequest = window.indexedDB.open('db-test');
+
+DBOpenRequest.onerror = event => console.error("Error loading database.");
+DBOpenRequest.onsuccess = event => {db = DBOpenRequest.result;};
+
+// writing to the database
+let transaction = db
+    .transaction(["test"], "readwrite")
+    .objectStore("test")
+    .add("test", 137);
+
+// reading from the database 
+let result;
+transaction.onsuccess(() => {
+    db.transaction(["test"])
+        .objectStore("test")
+        .get("test").onsuccess((event) => {
+        result = event.target.result;
+    })
+});
+```
+
+This project reduces this boiler plates to a nice single promise based class. This class allows you to think that you
+can access the IDBObjectStore class directly without having to think about creating transactions:
+
+```js
+import IndexeDBObjectStore from "IndexeDBObjectStore.ts"
+
+const store = await new IndexeDBObjectStore("my-db", "my-store")
+await store.put('test', 137)
+console.log(await store.get('test')) // 137
+```
+
+This is useful for instance when you need to store data from within a service worker/web worker. Using this class also
+makes it a lot harder to run into uncommited transactions.
+
+The class is only
+about ~200 lines of code, and you could take out the methods you don't need. This allows for just dropping the code in
+your project without having to install it as a dependency. There is a [simple-indexedb.ts](./simple-indexedb.ts)
+and [simple-indexedb.js](./simple-indexedb.js) version.
+
+## Interface:
+
+I have implemented most of the methods
+from [IDBObjectStore](https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore) but none of the cursor/index
+stuff (yet).
+
+```ts
+const key = "Test";
+const data = 137;
+const query = IDBKeyRange.lowerBound(100); // you can also pass strings as query
+
+import IndexeDBObjectStore from "IndexeDBObjectStore.ts"
+
+const store = await new IndexeDBObjectStore("my-db", "my-store")
+
+// Instance Methods:
+await store.add(key, data); // use to add key value pairs
+await store.put(key, data); // use to update key value pairs
+await store.get(key);
+await store.getJson(key);
+await store.delete(key);
+await store.getAll();
+await store.getAll(query);
+await store.getAll(query, count);
+await store.getKey(key);
+await store.getAllKeys(); // all keys
+await store.getAllKeys(query); // all keys that satisfy query
+await store.getAllKeys(query, count);
+await store.count();
+await store.count(query);
+await store.clear();
+
+// Properties
+console.log(store.db, store.dbname, store.objectstorename)
+```
+
+Feel free to submit a pull request.
---
+++ simple-indexedb.ts
@@ -0,0 +1,244 @@
+// @ts-nocheck
+function makePromise() {
+    let resolve;
+    let reject;
+    const promise = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej
+    })
+    return {
+        resolve,
+        reject,
+        promise
+    }
+}
+
+
+/**
+ * @class IndexeDBObjectStore
+ * A simple promise based wrapper for an indexedDB object store
+ * @param {string} dbname - The name of the database you want to open
+ * @param {string} dbname - The name of the objectstore you want to open
+ *
+ * You can have multiple databases each with multiple object stores. Each object store can hold key value pair objects.
+ * You can query these key value pairs using strings or using IDBKeyRange (https://developer.mozilla.org/en-US/docs/Web/API/IDBKeyRange)
+ */
+export class IndexeDBObjectStore {
+    db: IDBDatabase;
+    dbname: string;
+    objectstorename: string;
+
+    constructor(dbname: string, objectstorename: string) {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+        let request = indexedDB.open(dbname);
+        request.onerror = reject;
+        request.onsuccess = (event) => {
+            this.db = event.target.result;
+            resolve(this);
+        };
+        this.dbname = dbname;
+        this.objectstorename = objectstorename;
+        return promise;
+    }
+
+    /**
+     * Get an object store and parse it as json (if it was text)
+     * @param {string} key
+     */
+    async getJson(key: IDBValidKey) {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+
+        const transaction = this.db.transaction([this.objectstorename]).objectStore(this.objectstorename).get(key);
+
+        transaction.onsuccess = (event) => {
+            try {
+                let store = event.target.result;
+                if (typeof store === "undefined") throw `${key} is undefined`;
+                resolve(JSON.parse(store));
+            } catch (e) {
+                reject(`Could not parse ${key} in object store: ${e}`);
+            }
+        };
+        return promise;
+    }
+
+    /**
+     * Get the value stored under a key or key range
+     * @param {IDBValidKey | IDBKeyRange} key
+     */
+    async get(key: IDBValidKey | IDBKeyRange) {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+
+        const transaction = this.db.transaction([this.objectstorename]).objectStore(this.objectstorename).get(key);
+
+        transaction.onsuccess = (event) => {
+            try {
+                resolve(event.target.result);
+            } catch (e) {
+                reject(`Could not load ${name} store: ${e}`);
+            }
+        };
+        return promise;
+    }
+
+    /**
+     * Save data for a key in the store. Replaces data already there.
+     * @param {string} key
+     * @param {any} data
+     */
+    async put(key: IDBValidKey, data: any): Promise<string> {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+
+        const transaction = this.db.transaction([this.objectstorename], "readwrite").objectStore(this.objectstorename).put(data, key);
+
+        transaction.onsuccess = (event) => resolve(event.target.result);
+        transaction.onerror = reject;
+        return promise;
+    }
+
+    /**
+     * Add new key value pair to the store
+     * @param {string} key
+     * @param {any} value
+     */
+    async add(key: IDBValidKey, data: any) {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+
+        const transactiont = this.db.transaction([this.objectstorename], "readwrite").objectStore(this.objectstorename);
+        console.log(transactiont);
+        const transaction = transactiont.put(data, key);
+
+        transaction.onsuccess = (event) => resolve(event.target.result);
+        transaction.onerror = reject;
+        return promise;
+    }
+
+    /**
+     * Get all data in the object store. It is possible to query using
+     * [IDBKeyRange](https://developer.mozilla.org/en-US/docs/Web/API/IDBKeyRange)
+     * @param {undefined|IDBKeyRange} query - Optional query
+     * @param {undefined|number} count - Number of things to return
+     */
+    async getAll(query?: IDBKeyRange | IDBValidKey, count?: number): Promise<unknown> {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+        const transaction = this.db.transaction([this.objectstorename]).objectStore(this.objectstorename).getAll(query, count);
+
+        transaction.onsuccess = (event) => resolve(event.target.result);
+        transaction.onerror = reject;
+        return promise;
+    }
+
+    /**
+     * Clear the entire object store. **This removes all key value pairs in the object store**
+     */
+    clear(): Promise<undefined> {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+        const transaction = this.db.transaction([this.objectstorename], "readwrite").objectStore(this.objectstorename).clear();
+
+        transaction.onsuccess = (event) => resolve(event.target.result);
+        transaction.onerror = reject;
+        return promise;
+    }
+
+    /**
+     * Returns the number of key value pairs in the object store.
+     * You can also query count the results the query would get
+     * @param {undefined| IDBKeyRange | string} query
+     */
+    count(query?: IDBKeyRange | string): Promise<number> {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+        const transaction = this.db.transaction([this.objectstorename]).objectStore(this.objectstorename).count(query);
+
+        transaction.onsuccess = (event) => resolve(event.target.result);
+        transaction.onerror = reject;
+        return promise;
+    }
+
+    /**
+     * Get a indexed db request object for a certain key
+     * @param {IDBValidKey | IDBKeyRange} key
+     */
+    getKey(key: IDBValidKey | IDBKeyRange): Promise<string> {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+        const transaction = this.db.transaction([this.objectstorename]).objectStore(this.objectstorename).getKey(key);
+
+        transaction.onsuccess = (event) => resolve(event.target.result);
+        transaction.onerror = reject;
+        return promise;
+    }
+
+    /**
+     * Retrieves record keys for all objects in the object store matching the specified parameter or all objects in the store if no parameters are given.
+     * If you don't give arguments it returns all keys
+     * @param {IDBKeyRange|IDBValidKey} query
+     * @param {undefined|number} count
+     */
+    getAllKeys(query?: IDBKeyRange | IDBValidKey, count?: number): Promise<string[]> {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+        const transaction = this.db.transaction([this.objectstorename]).objectStore(this.objectstorename).getAllKeys();
+
+        transaction.onsuccess = (event) => resolve(event.target.result);
+        transaction.onerror = reject;
+        return promise;
+    }
+
+    /**
+     * Delete one specific key value pair from the object store.
+     * @param {IDBValidKey} key
+     */
+    delete(key: IDBValidKey | IDBKeyRange): Promise<undefined> {
+        const {
+            promise,
+            reject,
+            resolve
+        } = makePromise();
+        const transaction = this.db.transaction([this.objectstorename], "readwrite").objectStore(this.objectstorename).delete(key);
+
+        transaction.onsuccess = (event) => resolve(event);
+        transaction.onerror = reject;
+        return promise;
+    }
+
+}
+
+export default IndexeDBObjectStore;
