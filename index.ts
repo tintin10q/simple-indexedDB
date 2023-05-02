c805d3548208cc9da9f0dc39be836fe5020720eb
Quinten Cabo <tintin10q@hotmail.com>
2023-05-02 02:08:11 +0200

    Created index.ts which just imports the 2 main things `createIndexeddatabase` & IndexedDBObjectStore from the other files.
    Signed-off-by: Quinten Cabo <tintin10q@hotmail.com>

---
+++ index.ts
@@ -0,0 +1,9 @@
+
+
+import createIndexeddatabase from "./create-indexeddatabase";
+import IndexedDBObjectStore from "./simple-indexeddb";
+
+export {
+    createIndexeddatabase,
+    IndexedDBObjectStore
+}
